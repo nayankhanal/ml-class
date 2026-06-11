5ba6db28d1b01b3d5c16b1d11f84f483a68df89c
nayan <khanalnayan123@gmail.com>
2026-06-12 00:55:04 +0545

    multiple lr

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.title("Height Predictor")
 weight = st.number_input("Enter Weight")
 
 if st.button("Predict Height"):
-    # response = requests.post("http://localhost:8000/predict", json={'weight': weight})
-    response = requests.post("https://nayankhanal-height-predictor.hf.space/predict", json={'weight': weight})
+    response = requests.post("http://localhost:8000/predict", json={'weight': weight})
+    # response = requests.post("https://nayankhanal-height-predictor.hf.space/predict", json={'weight': weight})
     height = response.json()['predicted_height']
     # height = response
     st.success(f"Predicted Height: {height}")

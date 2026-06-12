252729417d472ac7f2a704fdb8e9e21f7deb8a30
nayan <khanalnayan123@gmail.com>
2026-06-13 02:45:22 +0545

    insurance dataset fro mlr

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+st.title("Insurace charge prediction website")
+st.write("Enter the details to predict insurance charges")
+
+# age = st.
+
+# all feature engineering to model training can be done here directly too
